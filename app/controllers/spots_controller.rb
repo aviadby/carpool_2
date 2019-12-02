17d2817cad0d36d729febf7b771337269779ad3e
Raghu Betina <raghu@firstdraft.com>
2019-12-02 20:16:02 +0000

    Only spot user can access show page

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,5 +1,5 @@
 class SpotsController < ApplicationController
-  before_action :current_user_must_be_spot_joiner, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_spot_joiner, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_spot_joiner
     spot = Spot.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
