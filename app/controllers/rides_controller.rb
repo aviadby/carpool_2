ea804fc0bc18991a1e8f7d7df856ba411ff259c7
Raghu Betina <raghu@firstdraft.com>
2019-12-02 20:16:02 +0000

    Only ride user can modify ride

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,4 +1,14 @@
 class RidesController < ApplicationController
+  before_action :current_user_must_be_ride_leader, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_ride_leader
+    ride = Ride.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == ride.leader
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @rides = Ride.all
 
