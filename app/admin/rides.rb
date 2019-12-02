d59ef2392cc1ff51892c25323721860c4acf115e
Raghu Betina <raghu@firstdraft.com>
2019-12-02 20:15:54 +0000

    Whitelist columns in Ride for ActiveAdmin

--- app/admin/rides.rb
+++ app/admin/rides.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Ride do
+
+ permit_params :origin, :destination, :departs, :ride_leader
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
