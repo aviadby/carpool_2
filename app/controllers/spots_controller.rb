1386a1f2a8c53af72a518c769f2f29bff6141a0a
Raghu Betina <raghu@firstdraft.com>
2019-12-02 20:16:02 +0000

    Only spot user can modify spot

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,4 +1,14 @@
 class SpotsController < ApplicationController
+  before_action :current_user_must_be_spot_joiner, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_spot_joiner
+    spot = Spot.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == spot.joiner
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @spots = Spot.all
 
